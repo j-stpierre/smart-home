77cb3a73f9a784ce8c8f06e6e8322e288053e4bc
James St-Pierre <jamesamstpierre@gmail.com>
2024-09-17 23:03:27 -0400

    Refactor rabbitmq into separate package, decouble triage from rabbitmq

---
+++ notifier/internal/rabbitmq/rabbitmq.go
@@ -0,0 +1,74 @@
+package rabbitmq
+
+import (
+	"errors"
+	"log"
+	"notifier/config"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+type RabbitMQ struct {
+	Conn    *amqp.Connection
+	Channel *amqp.Channel
+	Queue   string
+}
+
+func NewConsumer(cfg *config.Config) (*RabbitMQ, error) {
+
+	conn, err := amqp.Dial("amqp://" + cfg.RABBITMQUSER + ":" + cfg.RABBITMQPASS + "@localhost:" + cfg.RABBITMQPORT + "/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	q, err := ch.QueueDeclare(
+		"",    // name
+		false, // durable
+		false, // delete when unused
+		true,  // exclusive
+		false, // no-wait
+		nil,   // arguments
+	)
+	failOnError(err, "Failed to declare a queue")
+
+	err = ch.QueueBind(
+		q.Name,   // queue name
+		"",       // routing key
+		"events", // exchange
+		false,
+		nil,
+	)
+	failOnError(err, "Failed to bind a queue")
+
+	return &RabbitMQ{conn, ch, q.Name}, nil
+}
+
+func (r *RabbitMQ) Close() {
+	r.Conn.Close()
+	r.Channel.Close()
+}
+
+func (r *RabbitMQ) Consume() (<-chan amqp.Delivery, error) {
+	msgs, err := r.Channel.Consume(
+		r.Queue, // queue
+		"",      // consumer
+		true,    // auto-ack
+		false,   // exclusive
+		false,   // no-local
+		false,   // no-wait
+		nil,     // args
+	)
+
+	if err != nil {
+		log.Printf("Could not start consuming messages")
+		return nil, errors.New(err.Error())
+	}
+	return msgs, nil
+}
--- notifier/main.go
+++ notifier/main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	amqp "github.com/rabbitmq/amqp091-go"
 	"log"
 	"notifier/config"
+	"notifier/internal/rabbitmq"
 	"notifier/internal/triage"
 	"os"
 )
-
-func failOnError(err error, msg string) {
-	if err != nil {
-		log.Panicf("%s: %s", msg, err)
-	}
-}
 
 var port = os.Getenv("RABBITMQPORT")
 var username = os.Getenv("RABBITMQUSER")
@@ -22,56 +16,23 @@ var slackwebhook = os.Getenv("SLACKWEBHOOK")
 func main() {
 
 	cfg, err := config.LoadConfig()
+	if err != nil {
+		log.Printf("Error loading config")
+	}
 
-	//move to rabbit confi
-	conn, err := amqp.Dial("amqp://" + username + ":" + password + "@localhost:" + port + "/")
-	failOnError(err, "Failed to connect to RabbitMQ")
-	defer conn.Close()
+	r, err := rabbitmq.NewConsumer(cfg)
 
-	ch, err := conn.Channel()
-	failOnError(err, "Failed to open a channel")
-	defer ch.Close()
-
-	q, err := ch.QueueDeclare(
-		"",    // name
-		false, // durable
-		false, // delete when unused
-		true,  // exclusive
-		false, // no-wait
-		nil,   // arguments
-	)
-	failOnError(err, "Failed to declare a queue")
-
-	err = ch.QueueBind(
-		q.Name,   // queue name
-		"",       // routing key
-		"events", // exchange
-		false,
-		nil,
-	)
-	failOnError(err, "Failed to bind a queue")
-
-	msgs, err := ch.Consume(
-		q.Name, // queue
-		"",     // consumer
-		true,   // auto-ack
-		false,  // exclusive
-		false,  // no-local
-		false,  // no-wait
-		nil,    // args
-	)
-	failOnError(err, "Failed to register a consumer")
-
-	var forever chan struct{}
+	msgs, err := r.Consume()
 
 	go func() {
 		for d := range msgs {
+			log.Printf("Message received")
 			triage.Triage(cfg, d)
 		}
 	}()
 
 	log.Printf(" [*] Waiting for events. To exit press CTRL+C")
+	forever := make(chan bool)
 	<-forever
-	//end of rabbit config
 
 }
