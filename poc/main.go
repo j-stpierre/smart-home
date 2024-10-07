8eadc7d7ea7ebea3dbf376f7451700b70ff22b66
James St-Pierre <jamesamstpierre@gmail.com>
2024-10-06 22:35:01 -0400

    Add poc directory

---
+++ poc/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"context"
+	amqp "github.com/rabbitmq/amqp091-go"
+	"log"
+	"os"
+	"time"
+)
+
+func failOnError(err error, msg string) {
+	if err != nil {
+		log.Panicf("%s: %s", msg, err)
+	}
+}
+
+func main() {
+	port := os.Getenv("RABBITMQPORT")
+	username := os.Getenv("RABBITMQUSER")
+	password := os.Getenv("RABBITMQPASS")
+
+	conn, err := amqp.Dial("amqp://" + username + ":" + password + "@localhost:" + port + "/")
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	body := "No bin detected at curb"
+	err = ch.PublishWithContext(ctx,
+		"events", // exchange
+		"",       // routing key
+		false,    // mandatory
+		false,    // immediate
+		amqp.Publishing{
+			Headers: amqp.Table{
+				"source": "bindetector",
+			},
+			ContentType: "text/plain",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+
+	log.Printf(" [x] Sent %s", body)
+}
