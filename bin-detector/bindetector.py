8233334459f22e4de5b8393b41c426eade31d9b1
James St-Pierre <jamesamstpierre@gmail.com>
2024-10-09 00:24:07 -0400

    Add rabbitmq publish to bin detector and cron job

--- bin-detector/bindetector.py
+++ bin-detector/bindetector.py
@@ -1,8 +1,14 @@
 from ultralytics import YOLO
 import cv2
+import pika
+import os
+
+RABBITMQHOST = os.environ['RABBITMQDOMAIN']
+RABBITMQUSER = os.environ['RABBITMQUSER']
+RABBITMQPASS = os.environ['RABBITMQPASS']
 
 # Load YOLOv8 model
-model = YOLO('yolov8m_bin_trained.pt')  # Use 'yolov8n.pt' for nano model
+model = YOLO('yolov8m_bin_trained.pt')
 
 # Load an image
 image_path = 'pictures/captures/no-bin-day.png'
@@ -18,10 +24,21 @@ for box in result.boxes:
     if box.cls == 0:
         bin_found = True
 
-print(bin_found)
+# If False send message to rabbitmq
+if not bin_found:
 
+    credentials = pika.PlainCredentials(RABBITMQUSER, RABBITMQPASS)
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQHOST,credentials=credentials))
+    channel = connection.channel()
 
-# K8s job time frame for when to pull camera image
-# Pull camera image
-# Call inference function
-# If no bin detected produce event
+    headers = {
+    'source': 'bindetector',
+    }
+
+    properties = pika.BasicProperties(
+        headers=headers
+    )
+
+    channel.basic_publish(exchange='events', routing_key='', body='No bin detected', properties=properties)
+    print(" [x] Sent 'No bin detected'")
+    connection.close()
