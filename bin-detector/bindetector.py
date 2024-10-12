4cfd65ccd960f3fcb66470fc3aa51f0defee0dd2
James St-Pierre <jamesamstpierre@gmail.com>
2024-10-12 13:04:01 -0400

    Add http camera call from bin detector to capture image

--- bin-detector/bindetector.py
+++ bin-detector/bindetector.py
@@ -2,16 +2,29 @@ from ultralytics import YOLO
 import cv2
 import pika
 import os
+import requests
 
-RABBITMQHOST = os.environ['RABBITMQDOMAIN']
-RABBITMQUSER = os.environ['RABBITMQUSER']
-RABBITMQPASS = os.environ['RABBITMQPASS']
+RABBITMQHOST = os.environ["RABBITMQDOMAIN"]
+RABBITMQUSER = os.environ["RABBITMQUSER"]
+RABBITMQPASS = os.environ["RABBITMQPASS"]
+CAMERAURL = os.environ["CAMERAURL"]
+CAMERATOKEN = os.environ["CAMERATOKEN"]
 
 # Load YOLOv8 model
-model = YOLO('yolov8m_bin_trained.pt')
+model = YOLO("yolov8m_bin_trained.pt")
 
-# Load an image
-image_path = 'pictures/captures/no-bin-day.png'
+# Get image from camera
+headers = {"Authorization": "Bearer " + CAMERATOKEN}
+response = requests.get(CAMERAURL, headers=headers)
+
+print(response.status_code)
+
+if response.status_code == 200:
+    with open("pictures/captures/captured_image.jpg", "wb") as file:
+        file.write(response.content)
+        print("Image successfully saved as 'captured_image.jpg'")
+
+image_path = "pictures/captures/captured_image.jpg"
 image = cv2.imread(image_path)
 
 # Run inference
@@ -28,17 +41,19 @@ for box in result.boxes:
 if not bin_found:
 
     credentials = pika.PlainCredentials(RABBITMQUSER, RABBITMQPASS)
-    connection = pika.BlockingConnection(pika.ConnectionParameters(host=RABBITMQHOST,credentials=credentials))
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host=RABBITMQHOST, credentials=credentials)
+    )
     channel = connection.channel()
 
     headers = {
-    'source': 'bindetector',
+        "source": "bindetector",
     }
 
-    properties = pika.BasicProperties(
-        headers=headers
-    )
+    properties = pika.BasicProperties(headers=headers)
 
-    channel.basic_publish(exchange='events', routing_key='', body='No bin detected', properties=properties)
+    channel.basic_publish(
+        exchange="events", routing_key="", body="No bin detected", properties=properties
+    )
     print(" [x] Sent 'No bin detected'")
     connection.close()
